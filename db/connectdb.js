82c31864a6b0ee26820ea1b69d9f3079f5457fb9
Samz-9 <samyakjain08973@gmail.com>
2025-06-06 17:01:21 +0530

    uploaded

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,16 +1,60 @@
+// import mongoose from "mongoose";
+
+
+// const dbURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/RefugeeConnect';
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(dbURI);
+//         console.log('MongoDB connected...');
+//     } catch (err) {
+//         console.error(err.message);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 
+const MONGODB_URI = process.env.MONGO_URI;
 
-const dbURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/RefugeeConnect';
+if (!MONGODB_URI) {
+    throw new Error(
+        "Please define the MONGO_URI environment variable inside .env.local (or in Vercel settings)."
+    );
+}
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(dbURI);
-        console.log('MongoDB connected...');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn;
     }
-};
 
-export default connectDB;
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+}
+
+export default dbConnect;
